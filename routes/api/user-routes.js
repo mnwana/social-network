af4b0790ddde89cebadf73dd5e11d984fea47e9c
mnwana <mariellenwana@gmail.com>
2022-10-11 21:15:31 -0400

    add user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const { route } = require(".");
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} = require("../../controllers/user-controller");
+
+// /api/users
+router.route("/").get(getAllUsers).post(createUser);
+
+// /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// /api/users/:id
+router.route("/:userId/friends/:friendId").put(addFriend).delete(deleteFriend);
+
+module.exports = router;
