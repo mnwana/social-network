5ccad41328bcff252e93977fa8370d0cab5a3330
mnwana <mariellenwana@gmail.com>
2022-10-11 21:21:03 -0400

    add thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(createThought);
+
+// /api/thoughts/:id
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router
+  .route("/:thoughtId/reactions/")
+  .put(createReaction)
+  .delete(deleteReaction);
+
+module.exports = router;
